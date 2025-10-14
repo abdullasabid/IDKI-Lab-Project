8eb3ed806241080742ce63950cef287dbb6b1029
Abdulla Al Sabid <abdulla.sabid75@gmail.com>
2025-10-15 01:17:02 +0200

    Player Move left and right
    Move the player left and right.

--- LabProject/Assets/Scripts/PlayerController.cs
+++ LabProject/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 direction;
+    private Vector3 moveVector;  // Combined movement vector
     public float forwardSpeed;
+    public float laneDistance = 4f;
+    public float laneChangeSpeed = 5f;
+
+    private int desiredLane = 1; // 0:left, 1:middle, 2:right
+    private float targetX;       // Target x position for lane
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        forwardSpeed = 5f; // Set your forward speed here
+        targetX = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        direction.z = forwardSpeed;
+        // Input to change lanes
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane > 2) desiredLane = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane < 0) desiredLane = 0;
+        }
+
+        // Calculate target X position based on lane
+        targetX = (desiredLane - 1) * laneDistance;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        controller.Move(direction * Time.fixedDeltaTime);
+        // Calculate horizontal movement toward target lane smoothly
+        float deltaX = targetX - transform.position.x;
+        float horizontalMove = Mathf.Clamp(deltaX, -laneChangeSpeed * Time.fixedDeltaTime, laneChangeSpeed * Time.fixedDeltaTime);
+
+        // Build move vector: horizontal + forward
+        moveVector = new Vector3(horizontalMove, 0, forwardSpeed * Time.fixedDeltaTime);
+
+        // Move using CharacterController
+        controller.Move(moveVector);
     }
 }
